98a453ba30194345b822a417f003e478ecdf6cb1
djdron <djdron@gmail.com>
2017-07-21 19:01:35 +0300

    + https://www.foobar2000.org/components/view/foo_input_zxtune release

--- apps/foo_input_zxtune/src/foo_input_zxtune.cpp
+++ apps/foo_input_zxtune/src/foo_input_zxtune.cpp
@@ -57,7 +57,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
 // Note that it is possible to declare multiple components within one DLL, but it's strongly recommended to keep only one declaration per DLL.
 // As for 1.1, the version numbers are used by the component update finder to find updates; for that to work, you must have ONLY ONE declaration per DLL. If there are multiple declarations, the component is assumed to be outdated and a version number of "0" is assumed, to overwrite the component with whatever is currently on the site assuming that it comes with proper version numbers.
-DECLARE_COMPONENT_VERSION("ZXTune Player", "0.0.6",
+DECLARE_COMPONENT_VERSION("ZXTune Player", "0.0.7",
 "ZXTune Player (C) 2008 - 2016 by Vitamin/CAIG.\n"
 "based on r3750 dec 06 2016\n"
 "foobar2000 plugin by djdron (C) 2013 - 2017.\n\n"

ac011e4dabbd75c7da8530f62d2521150bfc60bb
djdron <djdron@gmail.com>
2020-12-01 18:54:28 +0200

    + foo_input_zxtune version increased to 0.0.9

--- apps/foo_input_zxtune/src/foo_input_zxtune.cpp
+++ apps/foo_input_zxtune/src/foo_input_zxtune.cpp
@@ -41,7 +41,7 @@ extern std::vector<PlayerPlugin::Ptr> player_plugins;
 // Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
 // Note that it is possible to declare multiple components within one DLL, but it's strongly recommended to keep only one declaration per DLL.
 // As for 1.1, the version numbers are used by the component update finder to find updates; for that to work, you must have ONLY ONE declaration per DLL. If there are multiple declarations, the component is assumed to be outdated and a version number of "0" is assumed, to overwrite the component with whatever is currently on the site assuming that it comes with proper version numbers.
-DECLARE_COMPONENT_VERSION("ZXTune Decoders", "0.0.8",
+DECLARE_COMPONENT_VERSION("ZXTune Decoders", "0.0.9",
 "ZXTune (C) 2008 - 2020 by Vitamin/CAIG.\n"
 "based on r4953 jul 23 2020\n"
 "foobar2000 plugin by djdron (C) 2013 - 2020.\n"
